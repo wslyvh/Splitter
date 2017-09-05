0b406cbedfecf3b9fda81e806fd97aac72b077d5
wslyvh <slewey@gmail.com>
2017-09-05 12:36:21 +0200

    Splitter

--- Contracts/Splitter.sol
+++ Contracts/Splitter.sol
@@ -1,13 +1,5 @@
 pragma solidity ^0.4.16;
 
-contract Mortal {
-    address owner;
-
-    function mortal() { owner = msg.sender; }
-
-    function kill() { if (msg.sender == owner) suicide(owner); }
-}
-
 contract Owned {
     address public owner; 
 
@@ -16,6 +8,38 @@ contract Owned {
     }
 }
 
-contract Splitter is Mortal, Owned {
+contract Mortal is Owned {
     
+    function kill() { 
+        if (msg.sender == owner) 
+            suicide(owner); 
+    }
+}
+
+
+contract Splitter is Mortal {
+    address public recipientA; 
+    address public recipientB; 
+    
+    function Splitter(address _recipientA, address _recipientB) {
+        require(recipientA != 0x0);
+        require(recipientB != 0x0);
+        
+        recipientA = _recipientA;
+        recipientB = _recipientB;
+    }
+    
+    function Split() payable returns(bool success) {
+        require(msg.value != 0);
+        
+        uint splitAmount = msg.value / 2;
+        uint remainder = msg.value - splitAmount * 2;
+        
+        recipientA.transfer(splitAmount);
+        recipientB.transfer(splitAmount);
+        
+        owner.transfer(remainder);
+
+        return true;
+    }    
 }
