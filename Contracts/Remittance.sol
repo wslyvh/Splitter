5cbf2b233e06c2646c1b3198dd57fded8e00833d
wslyvh <slewey@gmail.com>
2017-09-06 23:33:54 +0200

    Remittance

--- Contracts/Remittance.sol
+++ Contracts/Remittance.sol
@@ -17,8 +17,38 @@ contract Mortal is Owned {
 }
 
 contract Remittance is Mortal {
+    uint public deadline;
+    bytes32 hash;
     
-    function Remittance() {
+    function Remittance(uint _deadline, bytes32 _hash) {
+        require(_deadline > 0);
+        require(_hash > 0);
         
+        hash = _hash;
+    }
+    
+    function ReturnFunds() payable returns (bool success) {
+        require(msg.sender == owner);
+        require(block.timestamp > deadline);
+        
+        owner.transfer(this.balance);
+        return true;
+    }
+    
+    function Withdraw(uint _passwordA, uint _passwordB) payable returns (bool success) {
+        require(block.timestamp < deadline);
+        require(_passwordA > 0);
+        require(_passwordB > 0);
+        require(hash == keccak256(_passwordA == _passwordB));
+        
+        msg.sender.transfer(this.balance);
+        
+        return true;
+    }
+    
+    function TakeCut() payable returns (bool success) {
+        require(msg.sender == owner);
+        
+        return true;
     }
 }
